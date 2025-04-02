08aa654b4bf247974d51d43683f1b5e8204ea12b
Amsterdam19 <150171356+Amsterdam19@users.noreply.github.com>
2025-04-02 23:42:58 +0200

    alpha 7
    Astro Icon v1
    new map component
    sponsors component
    cool footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,57 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 //http://freibad-delligsen.de
 export default defineConfig({
-  site: 'http://freibad-delligsen.de',
-  integrations: [sitemap({
-    lastmod: new Date(),
-    serialize(item) {
-      if (/docs/.test(item.url)) {
-        return undefined;
-      }
-      if (/zeiten/.test(item.url)) {
-        item.changefreq = 'weekly';
-        item.lastmod = new Date();
-        item.priority = 0.9;
-      }
-      if (/preise/.test(item.url)) {
-        item.changefreq = 'weekly';
-        item.lastmod = new Date();
-        item.priority = 0.9;
-      }
-      if (/veranstaltungen/.test(item.url)) {
-        item.changefreq = 'weekly';
-        item.lastmod = new Date();
-        item.priority = 0.9;
-      }
-      if (/galerie/.test(item.url)) {
-        item.lastmod = new Date();
-        item.priority = 0.9;
-      }
-      if (/neues/.test(item.url)) {
-        item.changefreq = 'weekly';
-        item.lastmod = new Date();
-        item.priority = 0.7;
-      }
-      if (/impressum/.test(item.url)) {
-        item.priority = 0.4;
-      }
-      if (/mitglied/.test(item.url)) {
-        item.priority = 0.4;
-      }
-      if (/ueber/.test(item.url)) {
-        item.priority = 0.4;
-      }
-      return item;
-    },
-  }), tailwind()]
+  site: "http://freibad-delligsen.de",
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+      serialize(item) {
+        if (/docs/.test(item.url)) {
+          return undefined;
+        }
+        if (/zeiten/.test(item.url)) {
+          item.changefreq = "weekly";
+          item.lastmod = new Date();
+          item.priority = 0.9;
+        }
+        if (/preise/.test(item.url)) {
+          item.changefreq = "weekly";
+          item.lastmod = new Date();
+          item.priority = 0.9;
+        }
+        if (/veranstaltungen/.test(item.url)) {
+          item.changefreq = "weekly";
+          item.lastmod = new Date();
+          item.priority = 0.9;
+        }
+        if (/galerie/.test(item.url)) {
+          item.lastmod = new Date();
+          item.priority = 0.9;
+        }
+        if (/neues/.test(item.url)) {
+          item.changefreq = "weekly";
+          item.lastmod = new Date();
+          item.priority = 0.7;
+        }
+        if (/impressum/.test(item.url)) {
+          item.priority = 0.4;
+        }
+        if (/mitglied/.test(item.url)) {
+          item.priority = 0.4;
+        }
+        if (/ueber/.test(item.url)) {
+          item.priority = 0.4;
+        }
+        return item;
+      },
+    }),
+    tailwind(),
+    icon(),
+  ],
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,88 +1,88 @@
 //OSM
 function initOSM() {
-    const button = document.getElementById("load");
-    button.addEventListener("click", loadOSM);
+  const button = document.getElementById("load");
+  button.addEventListener("click", loadOSM);
 }
 function loadOSM() {
-    const iframe = document.createElement("iframe");
-    iframe.src =
-        "https://www.openstreetmap.org/export/embed.html?bbox=9.777574539184572%2C51.943762246255886%2C9.813408851623535%2C51.9556651936878&layer=mapnik&marker=51.94971411488452%2C9.795491695404053";
-    iframe.classList.add("osmap");
-    iframe.setAttribute("frameborder", "0");
-    iframe.setAttribute("scrollinng", "no");
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
-    iframe.style.borderRadius = "var(--indexBRadius)";
+  const iframe = document.createElement("iframe");
+  iframe.src =
+    "https://www.openstreetmap.org/export/embed.html?bbox=9.777574539184572%2C51.943762246255886%2C9.813408851623535%2C51.9556651936878&layer=mapnik&marker=51.94971411488452%2C9.795491695404053";
+  iframe.classList.add("osmap");
+  iframe.setAttribute("frameborder", "0");
+  iframe.setAttribute("scrollinng", "no");
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
 
-    //disable map preview
-    const mapPreview = document.querySelector(".mapPreview");
-    mapPreview.style.display = "none";
+  //disable map preview
+  const mapPreview = document.querySelector(".mapPreview");
+  mapPreview.style.display = "none";
+  document.getElementById("copy_badge").style.display = "none";
 
-    //show map Div
-    const map = document.getElementById("map");
-    map.style.visibility = "visible";
+  //show map Div
+  const map = document.getElementById("map");
+  map.style.display = "block";
 
-    //inject iframe
-    map.appendChild(iframe);
+  //inject iframe
+  map.appendChild(iframe);
 }
 
 //KurseCard
 function pfexpandCard() {
-    const pffab = document.getElementById("1fab");
-    const pfkurseCont = document.getElementById("1Cont");
-    pffab.classList.toggle("expandedFAB");
-    pfkurseCont.classList.toggle("expandedCont");
+  const pffab = document.getElementById("1fab");
+  const pfkurseCont = document.getElementById("1Cont");
+  pffab.classList.toggle("expandedFAB");
+  pfkurseCont.classList.toggle("expandedCont");
 }
 function brexpandCard() {
-    const brfab = document.getElementById("2fab");
-    const brkurseCont = document.getElementById("2Cont");
-    brfab.classList.toggle("expandedFAB");
-    brkurseCont.classList.toggle("expandedCont");
+  const brfab = document.getElementById("2fab");
+  const brkurseCont = document.getElementById("2Cont");
+  brfab.classList.toggle("expandedFAB");
+  brkurseCont.classList.toggle("expandedCont");
 }
 function initKurse() {
-    document.getElementById("1fab").addEventListener("click", () => {
-        pfexpandCard();
-    });
-    document.getElementById("2fab").addEventListener("click", () => {
-        brexpandCard();
-    });
+  document.getElementById("1fab").addEventListener("click", () => {
+    pfexpandCard();
+  });
+  document.getElementById("2fab").addEventListener("click", () => {
+    brexpandCard();
+  });
 }
 
 //Accordion
 function initAccordion() {
-    const accordion = document.querySelector(".accordion");
+  const accordion = document.querySelector(".accordion");
 
-    accordion.addEventListener("click", (e) => {
-        const activePanel = e.target.closest(".accordion-panel");
-        if (!activePanel) return;
-        toggleAccordion(activePanel);
+  accordion.addEventListener("click", (e) => {
+    const activePanel = e.target.closest(".accordion-panel");
+    if (!activePanel) return;
+    toggleAccordion(activePanel);
+  });
+  function toggleAccordion(panelToActivate) {
+    const buttons = panelToActivate.parentElement.querySelectorAll("button");
+    const contents =
+      panelToActivate.parentElement.querySelectorAll(".accordion-content");
+    const accordion_panel =
+      panelToActivate.parentElement.querySelectorAll(".accordion-panel");
+
+    buttons.forEach((button) => {
+      button.setAttribute("aria-expanded", false);
     });
-    function toggleAccordion(panelToActivate) {
-        const buttons = panelToActivate.parentElement.querySelectorAll("button");
-        const contents =
-            panelToActivate.parentElement.querySelectorAll(".accordion-content");
-        const accordion_panel =
-            panelToActivate.parentElement.querySelectorAll(".accordion-panel");
 
-        buttons.forEach((button) => {
-            button.setAttribute("aria-expanded", false);
-        });
+    contents.forEach((content) => {
+      content.setAttribute("aria-hidden", true);
+    });
 
-        contents.forEach((content) => {
-            content.setAttribute("aria-hidden", true);
-        });
+    accordion_panel.forEach((panel) => {
+      panel.classList.remove("imgActive");
+    });
 
-        accordion_panel.forEach((panel) => {
-            panel.classList.remove("imgActive");
-        });
+    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+    panelToActivate.classList.add("imgActive");
 
-        panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
-        panelToActivate.classList.add("imgActive");
-
-        panelToActivate
-            .querySelector(".accordion-content")
-            .setAttribute("aria-hidden", false);
-    }
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", false);
+  }
 }
 
 /* //Nav Scroll
@@ -110,7 +110,7 @@ function navSlide() {
 
 
     burger.addEventListener('click', () => {
-        
+
         //Toggle Nav
         nav.classList.toggle('nav-active');
         //aria-expanded
@@ -136,14 +136,15 @@ function navSlide() {
     })
 } */
 
-function init() {/* 
+function init() {
+  /*
     navScrollInit();
     setTimeout(navSlide, 1000); */
-    if (window.location.pathname === "/") {
-        setTimeout(initAccordion, 1000);
-        setTimeout(initOSM, 1000);
-        setTimeout(initKurse, 1000);
-    }
+  if (window.location.pathname === "/") {
+    setTimeout(initAccordion, 1000);
+    setTimeout(initOSM, 1000);
+    setTimeout(initKurse, 1000);
+  }
 }
 init();
-document.addEventListener('astro:after-swap', init);
+document.addEventListener("astro:after-swap", init);
