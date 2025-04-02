985ce22d5a6602786797aa99fab43031a4fc8fca
Amsterdam19 <150171356+Amsterdam19@users.noreply.github.com>
2025-04-03 00:26:14 +0200

    u

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#235fa4",
-          "secondary": "#143b64",
-          "accent": "#d41d54",
-          "neutral": "#E8EFF7",
-          "base-100": "#ffffff",
-          "info": "#5293dd",
-          "success": "#a3e635",
-          "warning": "#f59e0b",
-          "error": "#e11d48",
-          "--rounded-btn": "0.75rem",
-        },
-      },
-    ],
-  },
-
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+};
